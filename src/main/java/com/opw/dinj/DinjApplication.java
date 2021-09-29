777196f25130c788868d83a9e83616f5b8d0ff20
duyng2512 <duyng2512@gmail.com>
2021-09-30 02:13:10 +0700

    Commit init file

---
+++ src/main/java/com/opw/dinj/DinjApplication.java
@@ -0,0 +1,13 @@
+package com.opw.dinj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/opw/dinj/DinjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opw.dinj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
